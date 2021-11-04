aefa40f171835fb0b0d5b87ef61b2c473b4e015b
DemuraAIdev <cyberaioff@gmail.com>
2021-11-04 20:52:52 +0700

    Update

--- dotbot.sh
+++ dotbot.sh
@@ -13,9 +13,6 @@ else
     #download file
     cd .. && curl -L -o Lavalink.jar https://github.com/freyacodes/Lavalink/releases/download/3.4/Lavalink.jar
     echo "Lavalink downloaded start it"
-
-    terminal -e node $DIR/index.js
-    terminal -e java -jar $DIR/Lavalink.jar
 fi
 
 
