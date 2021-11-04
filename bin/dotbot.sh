84cdaacc61ec5961e89420f5659f7ab95af4bc7d
DemuraAIdev <cyberaioff@gmail.com>
2021-11-04 20:42:50 +0700

    ef

--- bin/dotbot.sh
+++ bin/dotbot.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. )"
+FILE=$DIR/Lavalink.jar
 if [ -f "$FILE" ]; then
     terminal -e node $DIR/index.js
-    terminal -e java -jar $DIR/lavalink.jar
+    terminal -e java -jar $DIR/Lavalink.jar
 else 
     echo "Lavalink doest exist"
     #download file
-    cd ..
-    curl -L -o Lavalink.jar https://github.com/freyacodes/Lavalink/releases/download/3.4/Lavalink.jar
+    cd .. && curl -L -o Lavalink.jar https://github.com/freyacodes/Lavalink/releases/download/3.4/Lavalink.jar
     echo "Lavalink downloaded start it"
 
     terminal -e node $DIR/index.js
-    terminal -e java -jar $DIR/lavalink.jar
+    terminal -e java -jar $DIR/Lavalink.jar
 fi
 
 
