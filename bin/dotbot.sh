e4fe808b8fb29ef1c6164b41c39a1aebdc6d0476
DemuraAIdev <cyberaioff@gmail.com>
2021-11-05 15:38:09 +0700

    Update script

--- bin/dotbot.sh
+++ bin/dotbot.sh
@@ -1,19 +1,23 @@
 #!/bin/sh
-#get current directory
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 #enter current directory
-cd $DIR
+cd $DIR 
+cd ..
 
 if [ -f "Lavalink.jar" ]; then
     #start new process
-    node $DIR/index.js
+    cd $DIR 
+    cd ..
+    node index.js
 else 
     echo "Lavalink doest exist"
     #download file
-    cd .. && curl -L -o Lavalink.jar https://github.com/freyacodes/Lavalink/releases/download/3.4/Lavalink.jar
+    cd $DIR 
+    cd ..
+    curl -L -o Lavalink.jar https://github.com/freyacodes/Lavalink/releases/download/3.4/Lavalink.jar
     echo "Lavalink downloaded start it in new tab"
-    node $DIR/index.js
+    node index.js
 fi
 
 
