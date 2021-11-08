08aaa54dfd1e68e4f8be3fb8468bf9b7d1569972
DemuraAIdev <cyberaioff@gmail.com>
2021-11-09 01:38:56 +0700

    fix some bugs

--- system/events/guildCreate.js
+++ system/events/guildCreate.js
@@ -1,13 +1,11 @@
 const { time } = require('@discordjs/builders');
 const moment = require("moment")
-let jsoning = require("jsoning");
-let db = new jsoning("database/global.json");
 const date = new Date();
 const timeString = time(date);
 module.exports = async (client, guild) => {
         console.info("Joined a new guild: " + guild.name);
         const global = await client.createnew()
-        const guild = client.guilds.cache.size.toLocaleString()
-        global.guildcount = guild
+        const guildcount = client.guilds.cache.size.toLocaleString()
+        global.guildcount = guildcount
         global.save()
 }
