50626b63d8d1e8da099b1d1b8a7ffeb59b6d78bd
DemuraAIdev <cyberaioff@gmail.com>
2021-11-12 00:32:33 +0700

    Docker Support

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM node:16.13.0
+# install python and make
+RUN apt-get update && \
+    apt-get install -y python3 build-essential && \
+    apt-get purge -y --auto-remove && \
+    apt-get install oracle-java17-installer
+WORKDIR /dotbot
+COPY package.json /dotbot
+RUN npm install
+COPY . /dotbot
+CMD [“node”, index.js”]
--- test/test.js
+++ test/test.js
@@ -1,1 +1,31 @@
-console.log("TEST COMPLETE")
+console.log("Test 1 run libs")
+require("../src/lib/console.debug")
+require("../src/lib/console.info")
+require("../src/lib/console.error")
+require("../src/lib/console.tips")
+require("../src/lib/console.warn")
+try {
+    console.debug("Test lib debug")
+    console.info("Test lib info")
+    console.error("Test lib error")
+    console.tips("Test lib tips")
+    console.warn("Test lib warn")
+} catch (e) {
+    console.error("Test 1 error", e)
+    console.log("Test 1 catch")
+    process.exit(0)
+}
+console.log("Test 1 end")
+console.log("Test 2 run")
+try {
+    const Util = require("../system/kernel/Util")
+    const util = new Util()
+    console.log("TEST2 :" + util.msToSec("1000"))
+    console.log("TEST2 :" + util.bytesToSize("10000", true))
+} catch (e) {
+    console.error("Test 2 error", e)
+    console.log("Test 2 catch")
+    process.exit(0)
+}
+console.log("Test 2 end")
+
